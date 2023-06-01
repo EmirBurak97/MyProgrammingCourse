b091ac1e0c55d961ff744b3be04fcc37003ef40d
Emir Burak <emirburakocakci@gmail.com>
2023-06-01 19:50:25 +0300

    Add files via upload

---
+++ Exceptions/Exceptions/Program.cs
@@ -0,0 +1,44 @@
+﻿//ExtecpcionsIntro();
+using Exceptions;
+
+try
+{
+    FindException();
+}
+catch (RecordNotFoundExceptions exception)
+{
+    Console.WriteLine(exception.Message);  
+}
+
+
+
+Console.ReadLine();
+
+static void ExtecpcionsIntro()
+{
+    try
+    {
+        string[] students = new string[3];
+        students[3] = "Emir";
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.InnerException);
+    }
+}
+
+static void FindException()
+{
+    List<string> students = new List<string>
+{
+    "Engin","Derin","Ada"
+};
+    if (!students.Contains("Ahmet"))
+    {
+        throw new RecordNotFoundExceptions();
+    }
+    else
+    {
+        Console.WriteLine("Record Founded.");
+    }
+}
---
+++ if-else/if-else/Program.cs
@@ -0,0 +1,15 @@
+﻿Random rnd = new Random();
+int sayi=rnd.Next(0,300);
+if (sayi>100 && sayi<200)
+{
+    Console.WriteLine(sayi + " 100 ile 200 arasındadır.");
+}
+else if (sayi>200)
+{
+    Console.WriteLine(sayi + " 200'den büyüktür.");
+}
+else
+{
+    Console.WriteLine(sayi + " 100'den küçüktür.");
+}
+Console.ReadLine();
