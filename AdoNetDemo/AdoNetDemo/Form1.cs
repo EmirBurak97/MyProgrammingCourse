db54b246a09c67b9006f59b4471f9a5f8beb29d8
Emir Burak <emirburakocakci@gmail.com>
2023-06-01 19:42:35 +0300

    Add files via upload

---
+++ AdoNetDemo/AdoNetDemo/Form1.cs
@@ -0,0 +1,67 @@
+namespace AdoNetDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+        ProductDal _productDal = new ProductDal();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadProduct();
+        }
+
+        private void LoadProduct()
+        {
+            dgwProducts.DataSource = _productDal.GetAll();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            _productDal.Add(new Product
+            {
+                Name = tbxName.Text,
+                UnitPrice = Convert.ToDecimal(tbxUnitPrice.Text),
+                StockAmount = Convert.ToInt32(tbxStockAmount.Text)
+            }
+            );
+
+            MessageBox.Show("Product Added!");
+        }
+
+        private void dgwProducts_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            tbxNameUpdate.Text = dgwProducts.CurrentRow.Cells[1].Value.ToString();
+            tbxStockAmountUpdate.Text = dgwProducts.CurrentRow.Cells[3].Value.ToString();
+            tbxUnitPriceUpdate.Text = dgwProducts.CurrentRow.Cells[2].Value.ToString();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            Product product = new Product
+            {
+                Id = Convert.ToInt32(dgwProducts.CurrentRow.Cells[0].Value),
+                Name = tbxNameUpdate.Text,
+                StockAmount = Convert.ToInt32(tbxStockAmountUpdate.Text),
+                UnitPrice = Convert.ToDecimal(tbxUnitPriceUpdate.Text)
+            };
+            _productDal.Update(product);
+            LoadProduct();
+            MessageBox.Show("Product Updated!");
+        }
+
+        private void btnRemove_Click_1(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(dgwProducts.CurrentRow.Cells[0].Value);
+            _productDal.Delete(id);
+            LoadProduct();
+            MessageBox.Show("Deleted!");
+        }
+    }
+}
