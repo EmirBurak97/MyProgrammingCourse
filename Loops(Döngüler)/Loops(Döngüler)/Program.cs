e92dfbaa20f6c28944d2dab83e68ab51c7cff252
Emir Burak <emirburakocakci@gmail.com>
2023-06-01 19:52:05 +0300

    Add files via upload

---
+++ Loops(Döngüler)/Loops(Döngüler)/Program.cs
@@ -0,0 +1,62 @@
+﻿//FOR Loop(FOR Döngüsü)
+
+//for (int i = 100; i >=0; i-=2)
+//{
+//    Console.WriteLine(i);
+//}
+//Console.WriteLine("It's Over.");
+//Console.WriteLine("----------");
+
+//------------------------------------------------------------------------
+
+//While Loop(While Döngüsü)
+
+//int sayi = 100;
+//while (sayi>=0)
+//{
+//    Console.WriteLine(sayi);
+//    sayi -= 2;
+
+//}
+//Console.WriteLine("It's Over.");
+//Console.WriteLine("----------");
+
+//------------------------------------------------------------------------
+
+//foreach loop(foreach döngüsü)
+//string[] students = new string[3] {"Ahmet","Mehmet","Fahri"};
+//foreach (string student in students)
+//{
+//    Console.WriteLine(student); 
+//}
+
+//------------------------------------------------------------------------
+
+// Asal mı değil mi?
+
+
+bool sonuc=false;
+Console.Write("Bir sayı giriniz : ");
+int eNum = int.Parse(Console.ReadLine());
+for (int i = 2; i < eNum; i++)
+{
+
+	if (eNum % i != 0)
+	{
+		sonuc = true;
+	}
+	else
+	{
+		sonuc = false;
+		i = eNum+1;	
+	}
+}
+if (sonuc==true)
+{
+	Console.WriteLine(eNum + " asal sayıdır.");
+}
+else
+{
+	Console.WriteLine(eNum + " asal sayı değildir.");
+}
+Console.ReadLine();
---
+++ methods/methods/Program.cs
@@ -0,0 +1,22 @@
+﻿Add();
+var result=Add2(34,53);
+Console.WriteLine(result);
+Console.WriteLine(Add3(2, 6, 7, 8, 9, 10));
+
+Console.ReadLine();
+static void Add()
+{
+    Console.WriteLine("Added!");
+}
+
+// Parametreli method
+static int Add2(int n1,int n2)
+{
+    var result = n1 + n2;
+    return result;
+}
+
+static int Add3(params int[] numbers)
+{
+    return numbers.Sum();
+}
