22601279a5bd8eb0e12e9fa6fdb1abaf808309c5
Emir Burak <emirburakocakci@gmail.com>
2023-06-01 19:51:20 +0300

    Add files via upload

---
+++ Inheritance/Inheritance/Program.cs
@@ -0,0 +1,29 @@
+﻿Person[] persons = new Person[2]
+{
+    new Customer{Name="Emir Burak"}, new Student{Name = "Hasan Emre"}
+};
+foreach (var person in persons)
+{
+    Console.WriteLine(person.Name);
+}
+
+Console.ReadLine();
+
+class Person
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string LastName { get; set; }
+}
+
+class Student : Person
+{
+    public string City { get; set; }
+}
+
+class Customer : Person
+{
+    public string City { get; set; }
+}
+
+
---
+++ Interfaces/Interfaces/Program.cs
@@ -0,0 +1,23 @@
+﻿using Interfaces;
+
+//InterfaceDemo();
+
+ICustomerDal[] customerDals = new ICustomerDal[2]
+{
+    new SqlServerCustomerDal(),
+    new OracleServerCustomerDal()
+};
+
+foreach (var customerDal in customerDals)
+{
+    customerDal.Add();
+}
+
+Console.ReadLine();
+
+static void InterfaceDemo()
+{
+    CustomerManager customerManager = new CustomerManager();
+    customerManager.Add(new SqlServerCustomerDal());
+    customerManager.Add(new OracleServerCustomerDal());
+}
