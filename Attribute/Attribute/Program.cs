1eb1a53ec4e55cfc17d2b05abd9d234a5d049f73
Emir Burak <emirburakocakci@gmail.com>
2023-06-01 19:43:14 +0300

    Add files via upload

---
+++ Attribute/Attribute/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Runtime.CompilerServices;
+
+Customer customer=new Customer { Id=1,LastName="Ocakci",Age=26};
+CustomerDal customerDal = new CustomerDal();
+customerDal.Add(customer);
+
+Console.ReadLine();
+
+class Customer
+{
+    public int Id { get; set; }
+    [RequiredProperty]
+    public string Name { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
+}
+
+class CustomerDal
+{
+    public void Add(Customer customer)
+    {
+        Console.WriteLine("{0},{1},{2},{3}",customer.Id,customer.Name,customer.LastName,customer.Age);
+    }
+}
+
+[AttributeUsage(AttributeTargets.Property,AllowMultiple = true)] // Required Property Attribute'una bir Attribute ekledik. Sadece nesnelerde kullanılmasını sağladık ve iki kere kullanılabilir olmasına izin verdik.
+class RequiredPropertyAttribute : Attribute
+{
+
+}
