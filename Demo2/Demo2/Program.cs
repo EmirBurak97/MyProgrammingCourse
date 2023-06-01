ef09c0b12cddc60c82cf3d258c78d7470472e34a
Emir Burak <emirburakocakci@gmail.com>
2023-06-01 19:48:17 +0300

    Add files via upload

---
+++ Demo2/Demo2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+CustomerManager customerManager = new CustomerManager();
+customerManager.Logger = new FileLogger();
+customerManager.Add();
+
+Console.ReadLine();
+
+class CustomerManager
+{
+    public ILogger Logger { get; set; }
+    public void Add()
+    {
+        Logger.Log();
+        Console.WriteLine("Customer Added.");
+    }
+}
+
+class DatabaseLogger: ILogger
+{
+    public void Log()
+    {
+        Console.WriteLine("Logged to database!");
+    }
+}
+
+class FileLogger : ILogger
+{
+    public void Log()
+    {
+        Console.WriteLine("Logged to file!");
+    }
+}
+
+interface ILogger
+{
+    void Log();
+}
