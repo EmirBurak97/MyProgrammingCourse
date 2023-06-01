fe268930fd72fee695b273337ba20c9d65bc70eb
Emir Burak <emirburakocakci@gmail.com>
2023-06-01 19:43:55 +0300

    Add files via upload

---
+++ Classes(Sınıflar)/Classes(Sınıflar)/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_Sınıflar_
+{
+    class Customer
+    {
+        public int Id { get; set; }
+
+        private string _name;
+        public string Name { 
+            get 
+            {
+                return "Ms. " + _name;
+            } 
+            set 
+            {
+                _name = value;
+            } 
+        }
+        public string LastName { get; set; }
+        public string City { get; set; }
+    }
+}
---
+++ Classes(Sınıflar)/Classes(Sınıflar)/Program.cs
@@ -0,0 +1,55 @@
+﻿using Classes_Sınıflar_;
+
+CustomerManager customerManager = new CustomerManager();
+customerManager.Add();
+customerManager.Update();
+
+ProductManager productManager = new ProductManager();
+productManager.Add();
+productManager.Update();
+
+Customer customer = new Customer();
+customer.Id = 1;
+customer.Name = "Emir Burak";
+customer.LastName = "Ocakci";
+customer.City = "Rize";
+
+//2.kullanım
+Customer customer2 = new Customer
+{
+    Id = 2,
+    Name = "Burak",
+    LastName = "Kulaber",
+    City = "Istanbul"
+};
+
+Console.WriteLine(customer.Name);
+Console.WriteLine(customer2.City);
+
+Console.ReadLine();
+
+class CustomerManager
+{
+    public void Add()
+    {
+        Console.WriteLine("Customer Added!");
+    }
+
+    public void Update()
+    {
+        Console.WriteLine("Customer Updated!");
+    }
+}
+
+class ProductManager
+{
+    public void Add()
+    {
+        Console.WriteLine("Product Added!");
+    }
+
+    public void Update()
+    {
+        Console.WriteLine("Product Updated!");
+    }
+}
